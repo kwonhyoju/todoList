97f2f459c11fd28bf08a094b50b589c960dea431
saung3754 <saung3754@nntuple.com>
2019-09-09 10:40:34 +0900

    todo.js - todoList localStorage에 저장

--- js/todo.js
+++ js/todo.js
@@ -3,19 +3,34 @@ const todoInput = todoForm.querySelector("input");
 const todoUl = document.querySelector("ul");
 
 const TODO_LS ="todoList";
+const arrayTodo =[];
+
+function saveLs (){
+    const todo_Ls = localStorage.setItem(TODO_LS,JSON.stringify(arrayTodo));
+}
 
 function paintTodo (text){
-    console.log(text);
     const createLi = document.createElement("li");
     const createSpan = document.createElement("span");
     const delBtn = document.createElement("button");
+    const createId = arrayTodo.length+1;
     createSpan.innerText = text;
     delBtn.innerText= "delete";
 
     createLi.appendChild(createSpan);
     createLi.appendChild(delBtn);
     todoForm.appendChild(createLi);
+
+    createLi.id= createId;
+
     todoInput.value='';
+
+    const todoObj ={
+        text,
+        id : createId
+    }
+    arrayTodo.push(todoObj);
+    saveLs();
 }
 
 function handleSubmit(event){
@@ -28,6 +43,10 @@ function todoValue (){
     const getTodo = localStorage.getItem(TODO_LS);
 
     if(getTodo !== null){
+        const parsedTodo=JSON.parse(getTodo);
+        parsedTodo.forEach((todo)=>{
+            paintTodo(todo.text);
+        });
 
     }
 }
