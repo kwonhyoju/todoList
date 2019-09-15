9edce58f55ab5af38dd262f5fdc04ce275f7f88e
saung3754 <saung3754@nntuple.com>
2019-09-15 23:05:17 +0900

    coordinate.js - localStorage에 날씨 저장 , weatherApi 호출 css 추가 수정

---
+++ js/coordinate.js
@@ -0,0 +1,54 @@
+const weatherSpan = document.querySelector(".js-weather");
+const API_KEY = "2b7de9006d7345a8230319eb6b9a8532";
+const LS_COORDS = "coords";
+
+function weather(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(json){   
+        return json.json();
+    }).then(function(json){
+        const location = json.name;
+        const temperature = json.main.temp;
+        weatherSpan.innerText= `${location}${temperature}`;
+    });
+}
+
+function saveGeoLocation(geo){
+    localStorage.setItem(LS_COORDS,JSON.stringify(geo));
+}
+
+function handleError(){
+    console.log("error");
+}
+
+function handleSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    succesGeoObj ={
+        latitude,
+        longitude,
+    }
+
+    saveGeoLocation(succesGeoObj);
+    weather(latitude,longitude);
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleSucces,handleError);
+}
+
+function loadLocation(){
+    const coords =localStorage.getItem(LS_COORDS);
+    if(coords === null){
+        askForCoords();    
+    } else {
+        const parseCoords = JSON.parse(coords);
+        weather(parseCoords.latitude,parseCoords.longitude);
+    }
+}
+
+function init(){
+    loadLocation();
+}
+
+init();
